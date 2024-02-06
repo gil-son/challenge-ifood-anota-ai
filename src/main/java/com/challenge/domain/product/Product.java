b5a8e917bef605e96bff584f0907cf94765d9032
gil-son <gilszon@gmail.com>
2024-02-06 11:13:42 -0300

    Domain

--- src/main/java/com/challenge/domain/product/Product.java
+++ src/main/java/com/challenge/domain/product/Product.java
@@ -1,2 +1,24 @@
-package com.challenge.domain.category.product;public class Product {
+package com.challenge.domain.product;
+
+import com.challenge.domain.category.Category;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collation = "products")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Product {
+
+    @Id
+    private String id;
+    private String title;
+    private String description;
+    private String ownerId;
+    private Integer price; // Multiply the price by 100 to remove the decimal point
+    private Category category;
+
 }
