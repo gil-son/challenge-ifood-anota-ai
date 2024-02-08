414c3347280f186331a2ba2dc3a6f25c1e0def35
gil-son <gilszon@gmail.com>
2024-02-08 09:51:04 -0300

    Override the ToString

--- src/main/java/com/challenge/domain/category/Category.java
+++ src/main/java/com/challenge/domain/category/Category.java
@@ -3,6 +3,7 @@ package com.challenge.domain.category;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.json.JSONObject;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -22,5 +23,17 @@ public class Category {
         this.title = categoryDTO.title();
         this.description = categoryDTO.description();
         this.ownerId = categoryDTO.ownerId();
+    }
+
+    @Override
+    public String toString(){
+        JSONObject json = new JSONObject();
+        json.put("title", this.title);
+        json.put("description", this.description);
+        json.put("ownerId", this.ownerId);
+        json.put("id", this.id);
+        json.put("type", "category");
+
+        return json.toString();
     }
 }
--- src/main/java/com/challenge/domain/product/Product.java
+++ src/main/java/com/challenge/domain/product/Product.java
@@ -1,9 +1,10 @@
 package com.challenge.domain.product;
 
-import com.challenge.domain.category.Category;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import netscape.javascript.JSObject;
+import org.json.JSONObject;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -19,12 +20,27 @@ public class Product {
     private String description;
     private String ownerId;
     private Integer price; // Multiply the price by 100 to remove the decimal point
-    private Category category;
+    private String category;
 
     public Product(ProductDTO productDTO){
         this.title = productDTO.title();
         this.description = productDTO.description();
         this.ownerId = productDTO.ownerId();
         this.price = productDTO.price();
+        this.category = productDTO.categoryId();
+    }
+
+    @Override
+    public String toString(){
+        JSONObject json = new JSONObject();
+        json.put("title", this.title);
+        json.put("description", this.description);
+        json.put("ownerId", this.ownerId);
+        json.put("id", this.id);
+        json.put("categoryId", this.category);
+        json.put("price", this.price);
+        json.put("type", "product");
+
+        return json.toString();
     }
 }
